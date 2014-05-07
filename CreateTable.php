78656cbf376fce275fa0115509a48ab1282d7bfb
Matthew McNaney <mcnaney@gmail.com>
2014-05-07 10:35:41 -0400

    Tidy code. Removed extra semi-colon.

--- CreateTable.php
+++ CreateTable.php
@@ -1,49 +1,47 @@
-<?php  
+<?php
 session_start();
 
 
-  $conn = pg_pconnect("host=localhost dbname=Message user=postgres password=****")
-		or die('Could not connect: ' . pg_last_error());;
-		if (!$conn)
-		{
-		echo "An error connection occurred.\n";
-		exit;
-		}
-   
-   $sql =<<<EOF
+
+$conn = pg_pconnect("host=localhost dbname=Message user=postgres password=****") or die('Could not connect: ' . pg_last_error());
+;
+if (!$conn) {
+    echo "An error connection occurred.\n";
+    exit;
+}
+
+$sql = <<<EOF
       CREATE TABLE Template3
       (ID INT PRIMARY KEY     NOT NULL,
       Keys           text[]   ,
 	  Values		 text[],
       Body            Text     NOT NULL
 	  );
-     
+
 EOF;
-   $ret = pg_query($conn, $sql);
-   if(!$ret){
-      echo pg_last_error($conn);
-   } else {
-      echo "Table created successfully\n";
-   }
-   
-   
-   $sql =<<<EOF
-     CREATE TABLE sentemail3(
-    ID INT      NOT NULL,
-    emailaddress  TEXT       NOT NULL,
+$ret = pg_query($conn, $sql);
+if (!$ret) {
+    echo pg_last_error($conn);
+} else {
+    echo "Table created successfully\n";
+}
+
+
+$sql = <<<EOF
+    CREATE TABLE sentemail3(
+    ID INT NOT NULL,
+    emailaddress  TEXT NOT NULL,
 	PRIMARY KEY(ID, emailaddress)
-            
 );
 EOF;
- $ret = pg_query($conn, $sql);
-   if(!$ret){
-      echo pg_last_error($conn);
-   } else {
-      echo "Table created successfully\n";
-   }
-   
-   
- 
-   pg_close($conn);
+$ret = pg_query($conn, $sql);
+if (!$ret) {
+    echo pg_last_error($conn);
+} else {
+    echo "Table created successfully\n";
+}
 
+
+
+pg_close($conn);
 ?>
