a6b76e5f11fefd4971c3d5156dacd5819817b1a7
Matthew McNaney <mcnaney@gmail.com>
2014-05-07 10:41:04 -0400

    Tidy code.

--- SelectTable.php
+++ SelectTable.php
@@ -1,77 +1,73 @@
-<?php  
-session_start(); 
+<?php
+
+session_start();
 // The main goal of this class is showing the template preview based on the user input.
-//Firs step is getting the content of the template file then substitute its keys with its real values.
+// First step is getting the content of the template file then substitute its keys with its real values.
 
-print_r( $json );
+print_r($json);
 
-  $conn = pg_pconnect("host=localhost dbname=Message user=postgres password=****")
-		or die('Could not connect: ' . pg_last_error());;
-		if (!$conn)
-		{
-		echo "An error connection occurred.\n";
-		exit;
-		}
-   
+$conn = pg_pconnect("host=localhost dbname=Message user=postgres password=****") or die('Could not connect: ' . pg_last_error());
+;
+if (!$conn) {
+    echo "An error connection occurred.\n";
+    exit;
+}
 
-    
- $sql3 =<<<EOF
+
+
+$sql3 = <<<EOF
           SELECT array_to_json(Keys),array_to_json(Values),Body FROM template3
-		
+
 	 ;
 EOF;
 
 $ret3 = pg_query($conn, $sql3);
-   if(!$ret3){
-      echo pg_last_error($conn);
-      exit;
-   };
-   
-   
-   
+if (!$ret3) {
+    echo pg_last_error($conn);
+    exit;
+};
+
+
+
 //$var = array();
- while($row3 = pg_fetch_row($ret3)){
-    //  echo "---ID = ". $row3[0] . "\n";  
-	//  echo "---Key = ". $row3[1] . "\n";  
-	//  echo "---Values = ". $row3[2] . "\n";  
-	//  echo "---Body = ". $row3[3] . "\n"; 
-	  
-//$templatefile=$row3[3];	
+while ($row3 = pg_fetch_row($ret3)) {
+    //  echo "---ID = ". $row3[0] . "\n";
+    //  echo "---Key = ". $row3[1] . "\n";
+    //  echo "---Values = ". $row3[2] . "\n";
+    //  echo "---Body = ". $row3[3] . "\n";
+//$templatefile=$row3[3];
 //$template = file_get_contents($templatefile);
 //echo $template;
 
-$vals = json_decode($row3[1]);
-$keys = json_decode($row3[0]);
-$variables = array();
+    $vals = json_decode($row3[1]);
+    $keys = json_decode($row3[0]);
+    $variables = array();
 
 
 //echo count($vals);
-	for($i = 0; $i < count($keys); ++$i) 
-	{
-		//echo $vals[$i];
-		$variables[$keys[$i]] = $vals[$i];
-	}
-	
-	
-/*	
-//******* JSON
-	$result = array();
-    array_push($result, array('keys'  => $keys,
-						 ));
-	echo json_encode(array("result" => $result));
+    for ($i = 0; $i < count($keys); ++$i) {
+        //echo $vals[$i];
+        $variables[$keys[$i]] = $vals[$i];
+    }
 
- //******** 
 
-*/
+    /*
+      //******* JSON
+      $result = array();
+      array_push($result, array('keys'  => $keys,
+      ));
+      echo json_encode(array("result" => $result));
 
+      //********
+
+     */
 }
 $template = file_get_contents("templatetext.html");
 
-foreach($variables as $key => $value)
-{
+foreach ($variables as $key => $value) {
 //echo $key;
 //echo $value;
-    $template = str_replace('{{ '.$key.' }}', $value, $template);
+    $template = str_replace('{{ ' . $key . ' }}', $value, $template);
 }
 
 //echo $template;
@@ -80,8 +76,7 @@ $_SESSION['template'] = $template;
 
 
 include 'TemplatePreview.php';
-  
 
-   pg_close($conn);
 
+pg_close($conn);
 ?>
