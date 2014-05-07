08810766683915358e913b197333b7a182e0314b
Matthew McNaney <mcnaney@gmail.com>
2014-05-07 10:39:02 -0400

    Tidy code.

--- RestForm1.php
+++ RestForm1.php
@@ -1,14 +1,15 @@
 <!DOCTYPE HTML>
-<html> 
-<body>
-<!--This form call insert function to insert the input data to the database. The difference between forms are mainly in this class.
- Each of the forms will have their own attribute and their own classes -->
-<form action="InsertTable.php" method="post">
-Fname: <input type="text" name="Fname"><br>
-Lname: <input type="text" name="Lname"><br>
-ID   : <input type="text" name="ID"><br>
-<input type="submit">
-</form>
+<html>
+    <body>
+        <!--This form calls an insert function to input the data into the database.
+            The difference between forms are mainly in this class.
+            Each of the forms will have their own attribute and their own classes -->
+        <form action="InsertTable.php" method="post">
+            Fname: <input type="text" name="Fname"><br>
+            Lname: <input type="text" name="Lname"><br>
+            ID   : <input type="text" name="ID"><br>
+            <input type="submit">
+        </form>
 
-</body>
+    </body>
 </html>
